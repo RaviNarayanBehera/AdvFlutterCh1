2955bb12e704a3164741eb20cb2b4fd3c01e0f36
RAVINARAYAN <ravinarayan.b9510@gmail.com>
2024-07-10 12:08:12 +0530

    10/7/24

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'component/Pages/stepper.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -19,50 +21,7 @@ class _MyAppState extends State<MyApp> {
       darkTheme: ThemeData.dark(),
       themeMode: isDark?ThemeMode.dark:ThemeMode.light,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Column(
-          children: [
-            const SizedBox(height: 220,),
-            const Center(child: Text('Yo Man!',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40),),),
-            const SizedBox(height: 15,),
-            const Text("It's a simple example of \n \t\t\t\t\t\t\t\t\t\tdark theme",style: TextStyle(fontSize: 25),),
-            const SizedBox(height: 35,),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  isDark = false;
-                });
-              },
-              child: Container(
-                height: 100,
-                width: 330,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.orangeAccent
-                ),
-                child: const Center(child: Text('Light Icon',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)),
-              ),
-            ),
-            const SizedBox(height: 20,),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  isDark = true;
-                });
-              },
-              child: Container(
-                height: 100,
-                width: 330,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.redAccent
-                ),
-                child: const Center(child: Text('Dark Icon',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)),
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: const StepperExample(),
     );
   }
 }
---
+++ lib/utils/globals.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+bool isGrid = false;
+int index = 0;
+var txtAccount = TextEditingController();
+var txtAddress = TextEditingController();
+var txtContact = TextEditingController();
